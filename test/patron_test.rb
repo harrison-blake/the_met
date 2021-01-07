5bf3959fdfe291cd0fb4f31c15b094a7e11a86a3
Harrison Blake <harrison.r.blake@gmail.com>
2021-01-07 09:13:29 -0700

    Finished patron attributes and instance_of test

---
+++ test/patron_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+
+class Patrontest < Minitest::Test
+  def test_patron_has_attributes
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_instance_of Patron, patron_1
+    assert_equal "Bob", patron_1.name
+    assert_equal 20, patron_1.spending_money
+  end
+end
